f8a13bcbf83df44bba348f248fdace4669670215
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-10-22 14:24:05 -0300

    M3#A16 - Adicionando mais contadores - testes

--- examples/07-react-counter/src/components/counter/counter.story.js
+++ examples/07-react-counter/src/components/counter/counter.story.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import React from 'react'
-import { Counter } from './index'
+import Counter from './index'
 import { action, storiesOf } from '@kadira/storybook'
 
 const stories = storiesOf('Counter', module)
